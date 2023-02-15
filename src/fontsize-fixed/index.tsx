11b1d8a012ff06a5be91ffbc2b99dcfb1b7127e9
zhangziyang <1136660162@qq.com>
2023-02-15 17:33:59 +0800

    add wx

--- src/fontsize-fixed/index.tsx
+++ src/fontsize-fixed/index.tsx
@@ -1,5 +1,4 @@
 import {defineComponent, ExtractPropTypes, onMounted, useSlots} from "vue";
-import './jweixin-1.6.0'
 
 const fontsizeFixedProps = {
     enable: Boolean
@@ -19,40 +18,46 @@ export type FontsizeFixedProps = ExtractPropTypes<typeof fontsizeFixedProps>
 export default defineComponent({
     name: 'fontsize-fixed',
     props: fontsizeFixedProps,
-    setup: () => {
-        const WeixinJSBridge = window.WeixinJSBridge;
-        onMounted(async () => {
-            var style = "body {\n" +
-                "  -webkit-text-size-adjust: 100% !important;\n" +
-                "  text-size-adjust: 100% !important;\n" +
-                "  -moz-text-size-adjust: 100% !important;\n" +
-                "}\n";
-            var ele = document.createElement("style");
-            ele.innerHTML = style;
-            document.getElementsByTagName('head')[0].appendChild(ele);
-            (function () {
-                console.log(WeixinJSBridge)
-                if (typeof WeixinJSBridge == 'object' && typeof WeixinJSBridge.invoke == 'function') {
-                    handleFontSize()
-                } else {
-                    if (document.addEventListener) {
-                        document.addEventListener('WeixinJSBridgeReady', handleFontSize, false)
-                    } else if (document.attachEvent) {
-                        document.attachEvent('WeixinJSBridgeReady', handleFontSize)
+    setup: (props) => {
+        const getInitFunc = () => {
+            if (props?.enable) {
+                var style = "body {\n" +
+                    "  -webkit-text-size-adjust: 100% !important;\n" +
+                    "  text-size-adjust: 100% !important;\n" +
+                    "  -moz-text-size-adjust: 100% !important;\n" +
+                    "}\n";
+                var ele = document.createElement("style");
+                ele.innerHTML = style;
+                document.getElementsByTagName('head')[0].appendChild(ele);
+                (function () {
+                    console.log(WeixinJSBridge)
+                    if (typeof WeixinJSBridge == 'object' && typeof WeixinJSBridge.invoke == 'function') {
+                        handleFontSize()
+                    } else {
+                        if (document.addEventListener) {
+                            document.addEventListener('WeixinJSBridgeReady', handleFontSize, false)
+                        } else if (document.attachEvent) {
+                            document.attachEvent('WeixinJSBridgeReady', handleFontSize)
 
-                        document.attachEvent('onWeixinJSBridgeReady', handleFontSize)
+                            document.attachEvent('onWeixinJSBridgeReady', handleFontSize)
+                        }
                     }
-                }
 
-                function handleFontSize() {
-                    // 设置网页字体为默认大小
-                    WeixinJSBridge.invoke('setFontSizeCallback', {fontSize: 0})
-                    // 重写设置网页字体大小的事件
-                    WeixinJSBridge.on('menu:setfont', () => {
+                    function handleFontSize() {
+                        // 设置网页字体为默认大小
                         WeixinJSBridge.invoke('setFontSizeCallback', {fontSize: 0})
-                    })
-                }
-            })()
+                        // 重写设置网页字体大小的事件
+                        WeixinJSBridge.on('menu:setfont', () => {
+                            WeixinJSBridge.invoke('setFontSizeCallback', {fontSize: 0})
+                        })
+                    }
+                })()
+            }
+
+
+        }
+        onMounted(async () => {
+            getInitFunc()
         })
         const slots = useSlots()
         return () => {
